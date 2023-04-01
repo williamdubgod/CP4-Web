4e4f3b107e5e1baac8048f40f07766205fb6797b
ViniKot <vini.kotches@gmail.com>
2023-03-31 21:01:02 -0300

    EX-7 cp4

--- script.js
+++ script.js
@@ -164,3 +164,21 @@ function Login() {
       alert("Usuário ou senha incorretos.")
   }
 }
+
+//ex 7-
+
+let idade = prompt("Digite a sua idade:");
+
+if (idade <= 12) {
+  console.log("Você é uma criança.");
+  alert("Você é uma criança.")
+} else if (idade <= 18) {
+  console.log("Você é um adolescente.");
+  alert("Você é um adolescente.")
+} else if (idade <= 60) {
+  console.log("Você é um adulto.");
+  alert("Você é um adulto.")
+} else {
+  console.log("Você é um idoso.");
+  alert("Você é um idoso.")
+}
